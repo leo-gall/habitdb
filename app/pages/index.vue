3604e10a26af972ab538bac0c7b2b74aa54e79bd
Leo Gall <leo@lgll.dev>
2025-12-11 22:24:36 +0100

    feat: add basic seo

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,3 +6,11 @@
     <IndexCallToAction />
   </div>
 </template>
+
+<script lang="ts" setup>
+useSeoMeta({
+  title: "Discover the Habits of David Goggins, Elon Musk, and More | HabitDB",
+  description:
+    "Explore and adopt the daily routines of top performers like David Goggins, Elon Musk, and others to boost your productivity and well-being with HabitDB.",
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,16 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/hints", "@nuxt/image"],
+  modules: ["@nuxt/eslint", "@nuxt/hints", "@nuxt/image", "@nuxtjs/seo"],
 
   css: ["~/main.css"],
   vite: {
     plugins: [tailwindcss()],
+  },
+  site: {
+    url: "https://habitdb.lgll.dev",
+    name: "HabitDB",
+    description: "Discover and Share the Best Habits to Transform Your Life",
+    defaultLocale: "en",
   },
 });
